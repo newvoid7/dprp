eb03e099569a971a9734f9ae8992c5470856b7b5
Link <xulizhan0302@outlook.com>
2023-12-14 11:04:48 +0800

    feat: add time log in profen and pps. doc: explain pps.best

--- pps.py
+++ pps.py
@@ -1,6 +1,8 @@
 from torch.nn import CosineSimilarity
 import numpy as np
 
+from utils import time_it
+
 
 class PPS:
     def __init__(self, probe_group, features, mask=None) -> None:
@@ -15,12 +17,21 @@ class PPS:
         self.sim_func = CosineSimilarity().cuda()
         self.mask = mask
         
-    def best(self, target, with_neighbor=True, alpha=0.5):
+    @time_it
+    def best(self, target, neighbor_factor=0.5):
+        """Find the best matching feature. Considering neighbors.
+        Args:
+            target (torch.Tensor): features of target
+            neighbor_factor (float, optional): How important the neighbor features are. 
+                Set to zero to omit neighbors. Defaults to 0.5.
+        Returns:
+            int: the index of the best matching feature in the probe group.
+        """
         similarity = self.sim_func(target, self.features)
-        if with_neighbor:
+        if neighbor_factor != 0:
             neighbors = similarity[np.asarray(self.probe_group.neighbor)]
             neighbors = neighbors.mean(-1)
-            similarity = alpha * similarity + (1 - alpha) * neighbors
+            similarity = (1 - neighbor_factor) * similarity + neighbor_factor * neighbors
         if self.mask is not None:
             # similarity - min() to keep minimum = 0, then set elements out of mask to 0 too
             similarity = self.mask * (similarity - similarity.min())

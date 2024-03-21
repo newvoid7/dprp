4fb7fb4f36812786828004e5bc7e26458af46680
newvoid7 <54870782+newvoid7@users.noreply.github.com>
2024-03-21 17:22:11 +0800

    feat: tracker init

---
+++ track.py
@@ -0,0 +1,7 @@
+class Tracker:
+    def __init__(self, first_image, first_label) -> None:
+        self.images = [first_image]
+        self.labels = [first_label]
+
+    def solve(next_image):
+        return
